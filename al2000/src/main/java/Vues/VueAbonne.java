75129900c6c8043169b6b31c88cbce6959c7eef3
Morgane-hen <57931653+Morgane-hen@users.noreply.github.com>
2020-11-23 14:51:49 +0100

    Create VueAbonne.java

---
+++ al2000/src/main/java/Vues/VueAbonne.java
@@ -0,0 +1,195 @@
+/**A Mettre a jour sur github**/
+
+package Vues;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VueAbonne extends JFrame{
+	
+	private static GestionnaireDeVues gestion = new GestionnaireDeVues();
+	private static JPanel panelCarte = new JPanel(); 
+	
+	VueAbonne(){
+		
+		super();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		BorderLayout blayout = new BorderLayout();
+		JPanel ensemble = new JPanel(blayout);
+		JPanel top_ecran = new JPanel();
+		JPanel accueil = new JPanel();
+		accueil.setLayout(null);
+		//top_ecran.setLayout(null);
+		
+		/////////////////////////Element de la vue///////////////////////////
+		JLabel titre = new JLabel("Bonjour **NomAbonne** !");
+		JButton retour = new JButton("Retour");
+		JButton help = new JButton("?");
+		JButton rendre = new JButton("Rendre DVD");
+		JButton films = new JButton("Voir Films");
+		JTextField recherche = new JTextField("Rechercher un film");
+		JButton recherche_al2000 = new JButton("in AL2000");
+		JButton recherche_cyber = new JButton("in Store");//Dans CyberVideo
+		JButton recharger_carte = new JButton("Recharger Carte");
+		JButton historique = new JButton("Consulter Historique");
+		JLabel rep_help = new JLabel("En cas de problème veuillez demander au gérant du store");
+		rep_help.setVisible(false);
+		
+		//////////////////////////////Action des boutons/////////////////////:
+		
+		//retourner à la page précedente
+		retour.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				gestion.vueAbonne.setVisible(false);
+				gestion.vueAccueil.setVisible(true);
+			}
+			
+		});
+		
+		//help : Affiche un message en bas de la fenetre
+		help.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				rep_help.setBounds(10, 550, rep_help.getPreferredSize().width, rep_help.getPreferredSize().height);
+				rep_help.setForeground(Color.BLUE);
+				rep_help.setVisible(true);
+				accueil.revalidate();		
+			}
+					
+		});
+		
+		//Le film est recherché parmis les films de la machine 
+		recherche_al2000.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String film = recherche.getText();
+				System.out.println("Le film a chercher est :"+film);
+			}
+			
+		});
+		
+		//Le film est recherché parmis les films de CyberVideo
+		recherche_cyber.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String film = recherche.getText();
+				System.out.println("Le film a chercher est :"+film);
+			}
+					
+		});
+		
+		//Afficher tout les films
+		films.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				gestion.vueAbonne.setVisible(false);
+				gestion.vueEnsembleFilm.setVisible(true);
+			}
+			
+		});
+		
+		//Recharger la carte
+		recharger_carte.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				accueil.setVisible(false);
+				ensemble.add(creerPanelCarte(), blayout.CENTER);
+				panelCarte.setVisible(true);
+				ensemble.revalidate();
+			}
+			
+		});
+		
+		//Voir l'historique 
+		historique.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+			
+		});
+		
+		///////////////Ajout des elements////////////////////////////:::
+		top_ecran.add(retour);
+		top_ecran.add(titre);
+		top_ecran.add(help);
+		accueil.add(recherche);
+		accueil.add(rendre);
+		accueil.add(recherche_al2000);
+		accueil.add(recherche_cyber);
+		accueil.add(films);
+		accueil.add(recharger_carte);
+		accueil.add(historique);
+		accueil.add(rep_help);
+		
+		////////////////////Placement des Elements//////////////////////
+		//Potentiellement a changer et a passer en Dynamique : pas forcement utile car taille de l'ecran ne change pas
+		Dimension d = new Dimension(titre.getPreferredSize());
+		titre.setBounds(300, 10, d.width, d.height);
+		d.setSize(retour.getPreferredSize());
+		retour.setBounds(10, 10, d.width, d.height);
+		d.setSize(help.getPreferredSize());
+		help.setBounds(740, 10, d.width, d.height);
+		d.setSize(recherche.getPreferredSize());
+		recherche.setBounds(150, 150, d.width+200, d.height);
+		recherche_al2000.setBounds(150+d.width+210, 150, d.width, d.height);
+		recherche_cyber.setBounds(150+d.width+350, 150, d.width, d.height);
+		d.setSize(films.getPreferredSize());
+		films.setBounds(300, 200, d.width, d.height);
+		d.setSize(rendre.getPreferredSize());
+		rendre.setBounds(300, 250, d.width, d.height);
+		d.setSize(recharger_carte.getPreferredSize());
+		recharger_carte.setBounds(300, 300, d.width, d.height);
+		d.setSize(historique.getPreferredSize());
+		historique.setBounds(300, 350, d.width, d.height);
+		
+		//////////////////////////:Dimension de la fenetre////////////////////////////
+		Dimension d_accueil = new Dimension(800, 600);
+		this.setMinimumSize(d_accueil);
+
+		ensemble.add(top_ecran, blayout.NORTH);
+		ensemble.add(accueil, blayout.CENTER);
+		this.add(ensemble);
+		pack();
+	}
+	
+	public JPanel creerPanelCarte() {
+		//Ajout des element 
+		JLabel l_montant = new JLabel("Quel montant voulez vous ajoutez :");
+		JTextField t_montant = new JTextField("tapez la somme que vous souhaitez ajouter a votre carte");
+		JButton confirmer = new JButton("Confirmer");
+		
+		//Action des boutons
+		confirmer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+			
+		});
+		
+		panelCarte.add(l_montant);
+		panelCarte.add(t_montant);
+		panelCarte.add(confirmer);
+		return panelCarte;
+	}
+}
