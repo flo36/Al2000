e3e5c9c4255a150b97bd7938e67cfa5f240e8fbc
JohanDa9u <wassimayari10@gmail.com>
2020-11-23 14:33:53 +0100

    Ajout table intermediaire Celebrite+Film

--- al2000/src/main/java/Cinema/Filmographie.java
+++ al2000/src/main/java/Cinema/Filmographie.java
@@ -1,6 +1,20 @@
 package cinema;
 
 public class Filmographie {
+
     private Celebrite celebrite; 
     private Film film;
+
+    public Filmographie(Celebrite celebrite, Film film){
+        this.celebrite = celebrite;
+        this.film = film;
+    }
+
+    public Celebrite getCelebrite(){
+        return this.celebrite;
+    }
+
+    public Celebrite getFilm(){
+        return this.film;
+    }
 }
