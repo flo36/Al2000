5ab692077e8f1f04f864f317fc7a38fe54530c66
flo36 <63344794+flo36@users.noreply.github.com>
2020-11-23 14:37:48 +0100

    Add files via upload

--- al2000/src/main/java/Client/Location.java
+++ al2000/src/main/java/Client/Location.java
@@ -1,5 +1,106 @@
 package Client;
 
-public class Location {
+import cinema.Dvd;
 
+/**
+	
+	getters et setters pour les attributs suivants :
+	- id_loc
+	- utilisateur
+	- dvd
+	
+	autorise_loc(Utilisateur u)
+	-> retourne un boolean qui autorise ou rejette une location sur les critères suivants : 
+	   - du nombre de location ACTUELLE de l'utilisateur
+	   - le solde de la carte d'abonne si c'est un abonne sinon toujours bon
+	   
+	ajoute_LocBdd(Utilisateur u)
+	-> ajoute la location dans la bdd, si c'est autorise 
+
+**/
+
+public class Location {
+	
+	private int id_loc = 0;
+	private Utilisateur user;
+	private Dvd dvd;
+	
+	public Location()
+	{
+		
+	}
+	
+	public Location(Utilisateur _user, Dvd _dvd)
+	{
+		this.user = _user;
+		this.dvd = _dvd;
+	}
+	
+/////////////////////////////////////debut getters && setters///////////////////////////////////////
+
+	
+	public Dvd getDvd() {
+		return dvd;
+	}
+	
+	public int getId_loc() {
+		return id_loc;
+	}
+	
+	public Utilisateur getUser() {
+		return user;
+	}
+	
+	public void setDvd(Dvd dvd) {
+		this.dvd = dvd;
+	}
+	
+	public void setId_loc(int id_loc) {
+		this.id_loc = id_loc;
+	}
+	
+	public void setUser(Utilisateur user) {
+		this.user = user;
+	}
+	
+/////////////////////////////////////fin getters && setters///////////////////////////////////////
+	
+	public boolean autorise_loc()
+	{
+		
+		//on regarde si l'utilisateur n'a pas déjà louer le nb max de dvd a la fois
+		if(!user.autorise_nb_loc())
+		{
+			//la location n'est pas autorisée, l'utilisateur a déjà atteint son nb de location en même temps
+			return false;
+		}
+		
+		//on regarde si le solde de l'utilisateur est suffisant pour louer un dvd
+		//pour le non abonne, il pourra toujours : on ne regarde pas son solde de cb
+		//pour l'abonne on regarde le solde de sa carte d'abonne
+		if(!user.solde_suffisant_loc())
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+
+	
+	public boolean ajoute_LocBdd()
+	{
+		//TODO : créer la requête pour  la location à la bdd
+		
+		boolean location_autorise = this.autorise_loc();
+		
+		if(location_autorise)
+		{
+			String valeurs = "";
+			
+			
+		}
+		
+		return location_autorise;	
+	}
 }
