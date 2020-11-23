8879a6586393e2c266d658e605f769dfd98c2b52
JohanDa9u <wassimayari10@gmail.com>
2020-11-23 13:10:16 +0100

    package cinema

--- al2000/src/main/java/cinema/Genre.java
+++ al2000/src/main/java/cinema/Genre.java
@@ -1,4 +1,24 @@
 package cinema;
 
-public class Genre {
+public enum Genre {
+    Action,
+    Adventure,
+    Comedy,
+    Crime,
+    Drama,
+    Fantasy,
+    Historical,
+    Horror,
+    Mystery,
+    Philosophical,
+    Political,
+    Romance,
+    Saga,
+    Satire,
+    Sf,
+    Social,
+    Speculative,
+    Thriller,
+    Urban,
+    Western
 }
