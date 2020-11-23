f32189fdd337e673bd6f65ddbb64022df6c66f7a
Morgane-hen <57931653+Morgane-hen@users.noreply.github.com>
2020-11-23 14:34:48 +0100

    Create GestionnaireDeVues.java

---
+++ al2000/src/main/java/Vues/GestionnaireDeVues.java
@@ -0,0 +1,11 @@
+package Vues;
+
+public class GestionnaireDeVues {
+
+	protected VueAccueil vueAccueil = new VueAccueil();
+	protected VueAbonne vueAbonne = new VueAbonne();
+	protected VueUser vueUser = new VueUser();
+	protected VueEnsembleFilm vueEnsembleFilm = new VueEnsembleFilm();
+	protected VueMaintenance vueMaintenance = new VueMaintenance();
+	
+}
