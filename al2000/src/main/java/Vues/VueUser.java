63969a05fb8f6f718b9bf118357b52c659e26ad8
Morgane-hen <57931653+Morgane-hen@users.noreply.github.com>
2020-11-27 19:02:10 +0100

    Update VueUser.java

--- al2000/src/main/java/Vues/VueUser.java
+++ al2000/src/main/java/Vues/VueUser.java
@@ -18,20 +18,14 @@ import Donnee.ALMediator;
 public class VueUser extends JFrame{
 	
 	private static GestionnaireDeVues gestion = new GestionnaireDeVues();
-	private ALMediator al = new ALMediator();
+	//private ALMediator al = new ALMediator();
 	
 	public VueUser(){
 		
 		super();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel user = new JPanel();
-		JPanel inscr = this.createPanelAbonnement();
-		inscr.setVisible(false);
 		JPanel accueil = new JPanel();
 		accueil.setLayout(null);
-		
-		user.add(inscr);
-		user.add(accueil);
 		
 		/////////////////////////Element de la vue///////////////////////////
 		JLabel titre = new JLabel("Bonjour !");
@@ -96,8 +90,8 @@ public class VueUser extends JFrame{
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				accueil.setVisible(false);
-				inscr.setVisible(true);;
+				gestion.vueUser.setVisible(false);
+				gestion.vueInscription.setVisible(true);
 			}
 			
 		});
@@ -107,7 +101,7 @@ public class VueUser extends JFrame{
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				al.rendreUnFilm();
+				//al.rendreUnFilm();
 			}
 			
 		});
@@ -145,7 +139,7 @@ public class VueUser extends JFrame{
 		Dimension d_accueil = new Dimension(800, 600);
 		this.setMinimumSize(d_accueil);
 
-		this.add(user);
+		this.add(accueil);
 		pack();
 	}
 	
