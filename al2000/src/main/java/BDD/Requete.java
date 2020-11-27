7680b1f2ece5fa4ef5aeabc7ed23b85129f899c3
Morgane-hen <57931653+Morgane-hen@users.noreply.github.com>
2020-11-27 17:36:51 +0100

    Ajout de la fonction de recherche generale

--- al2000/src/main/java/BDD/Requete.java
+++ al2000/src/main/java/BDD/Requete.java
@@ -1,7 +1,14 @@
 package BDD;
 
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
+
+import Cinema.Film;
+import Donnee.SQLException;
 
 import static BDD.Parser.resultSetToArray;
 
@@ -10,24 +17,24 @@ import static BDD.Parser.resultSetToArray;
   --JE CONSIDERE ICI QUE LES OBJETS "FILM" ET "USER" SONT PARSES SOUS FORMAT STRING--
 
  getFilmList()
- -> Récupere la liste des films
+ -> RÃ©cupere la liste des films
 
  getFilm(String nom)
- -> Recupère les données d'un film
+ -> RecupÃ¨re les donnÃ©es d'un film
 
  getAvailableFilmList(String nom)
- -> Récupere la liste des films stockés dans la machine
+ -> RÃ©cupere la liste des films stockÃ©s dans la machine
 
  modifyFilmAvailable(int id)
  -> emprunte ou rend un film
 
  addFilm(String nom)
- -> ajoute le DVD dans la liste (attention pas de vérification de quantité)
+ -> ajoute le DVD dans la liste (attention pas de vÃ©rification de quantitÃ©)
  deleteFilm(int id)
  -> supprime le DVD de la liste
 
  getUser(int id)
- -> récupère les infos d'un utilisateur
+ -> rÃ©cupÃ¨re les infos d'un utilisateur
 
  setUser(String user)
  -> inscrit les infos d'un utilisateur
@@ -84,6 +91,38 @@ public class Requete {
     public static int setUser(String user) {
         return new InteractionBaseOracle(USER_URL).sendUpdate("INSERT INTO LesClients VALUES (" + user + ")");
     }
+    
+    public ArrayList<Film> getFilms(String sql) {
+		InteractionBaseOracle bdd = new InteractionBaseOracle(
+				"jdbc:oracle:thin:@im2ag-oracle.e.ujf-grenoble.fr:1521/IM2AG", "nom_user", "mdp_user");
+
+
+		ArrayList<Film> films = new ArrayList<Film>();
+		
+		try {
+			bdd.connect();
+			ResultSet rs = bdd.sendRequest(sql);
+			while (rs.next()) {
+				String titre = rs.getString("titre");
+				DateFormat format = new SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH);
+				java.util.Date annee = null;
+				try {
+					annee = format.parse(rs.getString("annee"));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				int agemini = Integer.parseInt(rs.getString("agemini"));
+				String resume = rs.getString("synopsis");
+				int duree = Integer.parseInt(rs.getString("duree"));
+				films.add(new Film(titre,annee,agemini,resume,duree));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			bdd.disconnect();
+		}
+		return films;
+	}
 
 
 
